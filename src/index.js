1960248e3490ab7c75ab4922d7cc200e6a50f231
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-09-21 16:34:50 -0400

    Add form modal so we can get user data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css"
+
+const newTaskBtn = document.querySelector(".newTaskBtn")
+const newTaskModal = document.querySelector("#newTaskModal")
+const newTaskForm = document.querySelector("#newTaskForm")
+
+// main button event listeners
+
+newTaskBtn.addEventListener("click", () => newTaskModal.showModal())
+
+newTaskModal.addEventListener("click", e => {
+    const dialogDimensions = newTaskModal.getBoundingClientRect()
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+    newTaskModal.close()
+    newTaskForm.reset()
+    }
+  })
+
